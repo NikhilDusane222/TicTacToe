74a022483d32135e1f8d67db4df239a92c852be4
Nikhil <you@example.com>
2020-03-28 00:49:57 +0530

    Added: Assign the sign to player

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+X="X"
+O="O"
 declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
 
@@ -10,3 +12,11 @@ echo "-----------"
 echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
 echo "-----------"
 echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+
+letterCheck=$((RANDOM%2))
+case $letterCheck in
+	0) echo "You sign is : "$O
+		;;
+	1) echo "You sign is : "$X
+		;;
+esac
