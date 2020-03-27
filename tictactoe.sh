bf9ac05f63ee72463073a212417dcfe66f0aa258
Nikhil <you@example.com>
2020-03-28 00:27:48 +0530

    Added: Start fresh with resetting the board

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -x
 
-printf "Welcome to Tic Tac Toe game \n"
+declare -a gameBoard
+gameBoard=(" " " " " " " " " " " " " " " " " ")
 
+printf "Welcome to tic tac toe game \n"
+
+echo "${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+echo "-----------"
+echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+echo "-----------"
+echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
