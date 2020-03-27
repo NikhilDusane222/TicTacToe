f89be44d22be35de25bc917ddae0c13e4ac8430b
Nikhil <you@example.com>
2020-03-28 01:06:21 +0530

    Added: Display player turn and valid cells

--- tictactoe.sh
+++ tictactoe.sh
@@ -3,31 +3,67 @@
 #variables
 X="X"
 O="O"
+DOT=0
+CROSS=1
+USER=0
+COMPUTER=1
 declare -a gameBoard
 gameBoard=(" " " " " " " " " " " " " " " " " ")
 
 printf "Welcome to tic tac toe game \n"
-#board design
-echo "${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
-echo "-----------"
-echo "${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
-echo "-----------"
-echo "${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
 
-#assign the sign X or O
-letterCheck=$((RANDOM%2))
-case $letterCheck in
-	0) echo "You sign is : "$O
+function displayBoard()
+{
+	echo "Welcome to tic tac toe game"
+	echo "		${gameBoard[0]}  | ${gameBoard[1]} | ${gameBoard[2]}"
+	echo "		-----------"
+	echo "		${gameBoard[3]}  | ${gameBoard[4]} | ${gameBoard[5]}"
+	echo "		-----------"
+	echo "		${gameBoard[6]}  | ${gameBoard[7]} | ${gameBoard[8]}"
+}
+function playerTurn()
+{
+	playerLetter=$1
+	printf "Enter any number between 0 to 8 : "
+	read response
+	if [[ $response>=0 && $response<=8 ]]
+	then
+		if [[ "${gameBoard[$response]}"!=X || "${gameBoard[$response]}"!=O ]]
+		then
+			gameBoard[$response]="$playerLetter"
+		else
+			playerTurn $playerLetter
+		fi
+	else
+		printf "Invalid input \n"
+	fi
+	displayBoard
+}
+function getLetter()
+{
+	letterCheck=$((RANDOM%2))
+	case $letterCheck in
+		$DOT)
+			playerLetter=$O
 		;;
-	1) echo "You sign is : "$X
+		$CROSS)
+			playerLetter=$X
 		;;
-esac
-
-#first ture
-firstTurn=$((RANDOM%2))
-case $firstTurn in
-	0) echo "User plays first"
-	;;
-	1) echo "Computer plays first"
-	;;
-esac
+	esac
+	echo $playerLetter
+}
+function getTurn()
+{
+	firstTurn=$((RANDOM%2))
+	case $firstTurn in
+		$USER)
+			 printf "User plays first \n"
+		;;
+		$COMPUTER)
+			 printf "Computer plays first \n"
+		;;
+	esac
+}
+letter="$( getLetter)"
+getTurn
+playerTurn $letter
